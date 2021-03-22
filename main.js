fdf7a08004b1f55f1a099690e476dc4be282c90f
iaconom821 <iaconom821@gmail.com>
2021-03-22 19:45:44 -0400

    commit tree d62518b5004fe2f45e930e550fbec29528e67305

--- main.js
+++ main.js
@@ -6,6 +6,55 @@ const FULL_HEART = '♥'
 
 
 
+let error = document.querySelector('div#modal')
+let modal = document.querySelector('.hidden')
+
+let body = document.querySelector('body')
+
+let h2 = document.querySelector('h2')
+
+function errorRemoval() {
+  error.setAttribute('hidden', true)
+}
+
+function errorReplace() {
+  error.removeAttribute('hidden')
+  h2.innerText = 'Random Server Error Try Again'
+  setTimeout(function() {
+    errorRemoval()
+  }, 3000)
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  errorRemoval()
+})
+
+
+
+let article = document.querySelector('.like-glyph')
+
+article.addEventListener('click', function () {
+  let call = mimicServerCall()
+  call.then(function () {
+    let listSelector = document.querySelector('li')
+    let heartStatus = document.querySelector('li *')
+    if(heartStatus.id === 'highlighted'){
+      heartStatus.id = 'un-hilighted'
+      heartStatus.className = ''
+      heartStatus.innerHTML = EMPTY_HEART;
+      listSelector.replaceChild(heartStatus, heartStatus)
+    } else {
+      heartStatus.id = "highlighted"
+      heartStatus.className = 'activated-heart'
+      heartStatus.innerHTML = FULL_HEART;
+      listSelector.replaceChild(heartStatus, heartStatus)
+    }
+  }).catch(err => errorReplace())
+} )
+
+
+
+
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
